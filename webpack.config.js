038c5bcecbe6c7159f65ea16ebe38487d597a2c1
Bunk0vsky <jakub.luczynski.work@gmail.com>
2023-10-03 12:52:58 +0200

    webpack file name changed to index.html

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.html",
+  entry: "./index.html",
   output: {
     filename: "index_bundle.js",
     path: path.resolve(__dirname, "./build"),
