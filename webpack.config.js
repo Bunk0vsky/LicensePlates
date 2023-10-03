f85c90f5e31952fb4d72bd5d0d8eb8b38da69a0e
Bunk0vsky <jakub.luczynski.work@gmail.com>
2023-10-03 11:30:42 +0200

    webpack build changed

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./index.html",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.html",
     clean: true,
     assetModuleFilename: "[path][name][ext]",
