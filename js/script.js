543292b63ed0cedb48e1a98b42c78bbf08ab4fa1
Bunk0vsky <jakub.luczynski.work@gmail.com>
2023-10-06 11:14:15 +0200

    mini-menu fixed

--- js/script.js
+++ js/script.js
@@ -100,36 +100,35 @@ window.addEventListener("hashchange", function () {
   onLoadTranslate();
 });
 
-function counter() {
-  var counterContainer = document.querySelector(".website-counter");
-  var resetButton = document.querySelector("#reset");
-  var visitCount = localStorage.getItem("page_view");
+// function counter() {
+//   var counterContainer = document.querySelector(".website-counter");
+//   var resetButton = document.querySelector("#reset");
+//   var visitCount = localStorage.getItem("page_view");
 
-  // Check if page_view entry is present
-  if (visitCount) {
-    visitCount = Number(visitCount) + 1;
-    localStorage.setItem("page_view", visitCount);
-  } else {
-    visitCount = 1;
-    localStorage.setItem("page_view", 1);
-  }
-  counterContainer.innerHTML = visitCount;
+//   // Check if page_view entry is present
+//   if (visitCount) {
+//     visitCount = Number(visitCount) + 1;
+//     localStorage.setItem("page_view", visitCount);
+//   } else {
+//     visitCount = 1;
+//     localStorage.setItem("page_view", 1);
+//   }
+//   counterContainer.innerHTML = visitCount;
 
-  // Adding onClick event listener
-  // resetButton.addEventListener("click", () => {
-  //   visitCount = 1;
-  //   localStorage.setItem("page_view", 1);
-  //   counterContainer.innerHTML = visitCount;
-  // });
-}
+//   // Adding onClick event listener
+//   // resetButton.addEventListener("click", () => {
+//   //   visitCount = 1;
+//   //   localStorage.setItem("page_view", 1);
+//   //   counterContainer.innerHTML = visitCount;
+//   // });
+// }
 
-counter();
+// counter();
 
 //Make mobile navigation work
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
-
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
